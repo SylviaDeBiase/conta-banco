5acc95a2ad93b45a772acf816e4ab9b037554ca7
SylviaDeBiase <sylvia.h.biase@hotmail.com>
2024-10-21 19:00:56 -0300

    Desafio

---
+++ contaBanco/src/ContaTerminal.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        int numero;	
+        String agencia;
+        String cliente;
+        long saldo;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o nome do cliente!");
+        cliente = scanner.nextLine();
+        System.out.println("Digite o número da agência !");
+        agencia = scanner.nextLine();
+        System.out.println("Entre com o número da conta!");
+        numero = scanner.nextInt();
+        System.out.println("Qual o valor para abertura de conta?");
+        saldo = scanner.nextLong();
+
+        String mensagem = "Olá ".concat(cliente)
+            .concat(", obrigado por criar uma conta em nosso banco, sua agência é ")
+            .concat(agencia).concat(", conta ")
+            .concat(String.valueOf(numero)).concat(" e seu saldo ")
+            .concat(String.valueOf(saldo)).concat(" já está disponível para saque");
+
+        System.out.println(mensagem);
+    }
+}
